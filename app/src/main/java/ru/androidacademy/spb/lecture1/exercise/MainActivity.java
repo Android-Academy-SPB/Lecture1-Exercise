85f2f36f0dad063ce41e114d1f3c33ec7ae26b47
Artur Vasilov <avasilov@yandex-team.ru>
2018-03-25 23:45:52 +0300

    Added email app integration

--- app/src/main/java/ru/androidacademy/spb/lecture1/exercise/MainActivity.java
+++ app/src/main/java/ru/androidacademy/spb/lecture1/exercise/MainActivity.java
@@ -1,12 +1,17 @@
 package ru.androidacademy.spb.lecture1.exercise;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String YOUR_EMAIL = null; // TODO : put your email here
 
     private EditText messageEdit;
 
@@ -39,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void openEmail() {
+        String message = messageEdit.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"))
+                .putExtra(Intent.EXTRA_EMAIL, YOUR_EMAIL)
+                .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_subject))
+                .putExtra(Intent.EXTRA_TEXT, message);
 
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.email_sending_error, Toast.LENGTH_LONG).show();
+        }
     }
 }
